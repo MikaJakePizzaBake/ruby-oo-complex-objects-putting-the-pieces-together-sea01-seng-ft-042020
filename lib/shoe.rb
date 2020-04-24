0ea06bfc275fcb6112c0c178f7ad31e556eb55ee
Trevor Low <tlow94@uw.edu>
2020-04-24 01:51:34 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,9 @@
-
+class shoe
+  atrr_accessor :color, :size, :material, :condition
+  def initialize
+    @color = color
+    @size = size
+    @material = material
+    @condition = condition
+  end
+end
